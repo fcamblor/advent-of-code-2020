f01499265e67dbdc83806b37d8665a98393f7c15
fcamblor <fcamblor@gmail.com>
2020-12-12 11:22:26 +0100

    D12 part 2 rough impl

--- src/2020-12.ts
+++ src/2020-12.ts
@@ -19,9 +19,9 @@ const MOVE_WEST="W";
 
 type Action = (typeof MOVE_NORTH)|(typeof MOVE_WEST)|(typeof MOVE_EAST)|(typeof MOVE_SOUTH)|(typeof FORWARD)|(typeof TURN_LEFT)|(typeof TURN_RIGHT);
 
+
+
 type State = { coord: ShipCoord, direction: Direction };
-
-
 const RUNNER: Record<Action, {move: (num: number, state: State) => State}> = {
     N: { move: (num, state) => ({ ...state, coord: { x: state.coord.x, y: state.coord.y+num } }) },
     S: { move: (num, state) => ({ ...state, coord: { x: state.coord.x, y: state.coord.y-num } }) },
@@ -31,8 +31,6 @@ const RUNNER: Record<Action, {move: (num: number, state: State) => State}> = {
     L: { move: (num, state) => ({ ...state, direction: DIRECTIONS[ (DIRECTIONS.indexOf(state.direction) - (num/90) + DIRECTIONS.length)%DIRECTIONS.length ] }) },
     F: { move: (num, state) => RUNNER[state.direction].move(num, state) },
 };
-
-
 export class D11Ship {
     private state: State = {
         coord: {x:0, y:0},
@@ -45,6 +43,55 @@ export class D11Ship {
         const match = command.match(/^([A-Z])([0-9]+)$/);
         const [ action, num ] = [ match![1] as Action, Number(match![2]) ];
         this.state = RUNNER[action].move(num, this.state);
+        return this.currentState();
+    }
+
+    manhattanDistance() {
+        return Math.abs(this.state.coord.x)+Math.abs(this.state.coord.y);
+    }
+
+    currentState() {
+        return {...this.state};
+    }
+}
+
+
+type Waypoint = { stepX: number, stepY: number };
+type State2 = { coord: ShipCoord, waypoint: Waypoint };
+const RUNNER2: Record<Action, {move: (num: number, state: State2) => State2}> = {
+    N: { move: (num, state) => ({ ...state, waypoint: { stepY: state.waypoint.stepY + num, stepX: state.waypoint.stepX } }) },
+    S: { move: (num, state) => ({ ...state, waypoint: { stepY: state.waypoint.stepY - num, stepX: state.waypoint.stepX } }) },
+    W: { move: (num, state) => ({ ...state, waypoint: { stepY: state.waypoint.stepY, stepX: state.waypoint.stepX - num } }) },
+    E: { move: (num, state) => ({ ...state, waypoint: { stepY: state.waypoint.stepY, stepX: state.waypoint.stepX + num } }) },
+    F: { move: (num, state) => ({ ...state, coord: { x: state.coord.x + num*state.waypoint.stepX, y: state.coord.y + num*state.waypoint.stepY} }) },
+    R: { move: (num, state) => ({ ...state, waypoint: applyRotation(state.waypoint, "CLOCKWISE", num/90) }) },
+    L: { move: (num, state) => ({ ...state, waypoint: applyRotation(state.waypoint, "COUNTER_CLOCKWISE", num/90) }) }
+};
+
+type RotationType = "CLOCKWISE"|"COUNTER_CLOCKWISE";
+const ROTATIONS_TRANSFORMS: Record<RotationType, (waypoint: Waypoint) => Waypoint> = {
+    CLOCKWISE: (waypoint: Waypoint) => ({ stepX: waypoint.stepY, stepY: -waypoint.stepX }),
+    COUNTER_CLOCKWISE: (waypoint: Waypoint) => ({ stepX: -waypoint.stepY, stepY: waypoint.stepX }),
+}
+const applyRotation = (waypoint: Waypoint, rotationType: RotationType, times: number) => {
+    let result: Waypoint = waypoint;
+    for(var i=0; i<times; i++) {
+        result = ROTATIONS_TRANSFORMS[rotationType](result);
+    }
+    return result;
+}
+export class D11Ship2 {
+    private state: State2 = {
+        coord: {x:0, y:0},
+        waypoint: {stepX: 10, stepY: 1}
+    };
+    constructor() {
+    }
+
+    move(command: string): State2 {
+        const match = command.match(/^([A-Z])([0-9]+)$/);
+        const [ action, num ] = [ match![1] as Action, Number(match![2]) ];
+        this.state = RUNNER2[action].move(num, this.state);
         return this.currentState();
     }
 
--- test/2020-12.test.ts
+++ test/2020-12.test.ts
@@ -1,6 +1,6 @@
 import {readLines} from "../src/utils";
 import {INPUT, Q1_SAMPLE} from "./2020-12.inputs";
-import {D11Ship, DIRECTIONS} from "../src/2020-12";
+import {D11Ship, D11Ship2, DIRECTIONS} from "../src/2020-12";
 
 
 test('Q1 sample', () => {
@@ -17,4 +17,82 @@ test('Q1', () => {
         ship.move(command);
     })
     expect(ship.manhattanDistance()).toBe(2270);
+})
+
+
+test('Q2 rotates', () => {
+    let ship = new D11Ship2();
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 1});
+    expect(ship.currentState().coord).toEqual({x:0, y: 0});
+
+    ship.move("F10");
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 1});
+    expect(ship.currentState().coord).toEqual({x:100, y: 10});
+
+    ship.move("N3");
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 4});
+    expect(ship.currentState().coord).toEqual({x:100, y: 10});
+
+    ship.move("F7");
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 4});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("R90");
+    expect(ship.currentState().waypoint).toEqual({stepX:4, stepY: -10});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("R90");
+    expect(ship.currentState().waypoint).toEqual({stepX: -10, stepY: -4});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("R90");
+    expect(ship.currentState().waypoint).toEqual({stepX: -4, stepY: 10});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("L90");
+    expect(ship.currentState().waypoint).toEqual({stepX: -10, stepY: -4});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("L90");
+    expect(ship.currentState().waypoint).toEqual({stepX: 4, stepY: -10});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("L90");
+    expect(ship.currentState().waypoint).toEqual({stepX: 10, stepY: 4});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+})
+
+test('Q2 sample', () => {
+    let ship = new D11Ship2();
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 1});
+    expect(ship.currentState().coord).toEqual({x:0, y: 0});
+
+    ship.move("F10");
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 1});
+    expect(ship.currentState().coord).toEqual({x:100, y: 10});
+
+    ship.move("N3");
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 4});
+    expect(ship.currentState().coord).toEqual({x:100, y: 10});
+
+    ship.move("F7");
+    expect(ship.currentState().waypoint).toEqual({stepX:10, stepY: 4});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("R90");
+    expect(ship.currentState().waypoint).toEqual({stepX:4, stepY: -10});
+    expect(ship.currentState().coord).toEqual({x:170, y: 38});
+
+    ship.move("F11");
+    expect(ship.currentState().waypoint).toEqual({stepX:4, stepY: -10});
+    expect(ship.currentState().coord).toEqual({x:214, y: -72});
+})
+
+test('Q2', () => {
+    let ship = new D11Ship2();
+    readLines(INPUT).forEach(command => {
+        ship.move(command);
+    })
+    expect(ship.manhattanDistance()).toBe(138669);
 })
