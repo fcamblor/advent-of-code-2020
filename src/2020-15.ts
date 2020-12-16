0819bd0e6ff6cb4c9d341aeee379e175e8784760
fcamblor <fcamblor@gmail.com>
2020-12-16 11:55:28 +0100

    put back listOfSpoken number, but using push() instead of concat() to avoid copying the whole array at each loop execution

--- src/2020-15.ts
+++ src/2020-15.ts
@@ -3,12 +3,15 @@ import {extractColumnBasedValues, reduceRange, reduceTimes, rotateMatrix} from "
 export function q15Read(str: string) { return str.split(",").map(Number); }
 
 export function findNumbersAfter(startingNumbers: number[], requestedTurn: number) {
-    const { perNumberLastSpokenIndex } = startingNumbers.reduce(({ perNumberLastSpokenIndex }, num, index) => {
+    const { perNumberLastSpokenIndex } = startingNumbers.reduce(({ perNumberLastSpokenIndex, listOfSpokenNumbers }, num, index) => {
         perNumberLastSpokenIndex.set(num, index+1);
-        return { perNumberLastSpokenIndex };
-    }, { perNumberLastSpokenIndex: new Map() as Map<number,number> })
+        listOfSpokenNumbers.push(num);
+        return { perNumberLastSpokenIndex, listOfSpokenNumbers };
+    }, { perNumberLastSpokenIndex: new Map() as Map<number,number>, listOfSpokenNumbers: [] as number[] })
 
-    const { lastSpokenNumber } = reduceRange(startingNumbers.length + 1, requestedTurn, ({ perNumberLastSpokenIndex, perNumberAnteLastSpokenIndex, lastSpokenNumber }, turn) => {
+    const { listOfSpokenNumbers } = reduceRange(startingNumbers.length + 1, requestedTurn, ({ listOfSpokenNumbers, perNumberLastSpokenIndex, perNumberAnteLastSpokenIndex }, turn) => {
+        const lastSpokenNumber = listOfSpokenNumbers[listOfSpokenNumbers.length-1];
+
         let numberToSay;
         if(!perNumberAnteLastSpokenIndex.has(lastSpokenNumber)) {
             numberToSay = 0;
@@ -20,13 +23,14 @@ export function findNumbersAfter(startingNumbers: number[], requestedTurn: numbe
             perNumberAnteLastSpokenIndex.set(numberToSay, perNumberLastSpokenIndex.get(numberToSay)!);
         }
         perNumberLastSpokenIndex.set(numberToSay, turn);
+        listOfSpokenNumbers.push(numberToSay);
 
-        return { perNumberLastSpokenIndex, perNumberAnteLastSpokenIndex, lastSpokenNumber: numberToSay };
-    }, { perNumberLastSpokenIndex, perNumberAnteLastSpokenIndex: new Map() as Map<number,number>, lastSpokenNumber: startingNumbers[startingNumbers.length-1] });
+        return { listOfSpokenNumbers, perNumberLastSpokenIndex, perNumberAnteLastSpokenIndex, lastSpokenNumber: numberToSay };
+    }, { listOfSpokenNumbers: startingNumbers as number[], perNumberLastSpokenIndex, perNumberAnteLastSpokenIndex: new Map() as Map<number,number> });
 
-    return { lastSpokenNumber };
+    return { lastSpokenNumber: listOfSpokenNumbers[listOfSpokenNumbers.length-1], listOfSpokenNumbers };
 }
 
 function SHOW_Q1_LIST_OF_SPOKEN_NUMBERS(list: string) {
-    // return rotateMatrix([ findNumbersAfter(q15Read(list), 2020).listOfSpokenNumbers ]);
+    return rotateMatrix([ findNumbersAfter(q15Read(list), 2020).listOfSpokenNumbers ]);
 }
