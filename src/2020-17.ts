868dfbe4e1dab47e757ed4439d47d0cd10883dd1
fcamblor <fcamblor@gmail.com>
2020-12-17 08:30:58 +0100

    D17 part2 crappy copy paste, refactoring incoming...

--- src/2020-17.ts
+++ src/2020-17.ts
@@ -82,3 +82,82 @@ export class Pocket3Dimension {
         return `${coord.x}_${coord.y}_${coord.z}`;
     }
 }
+
+type D174DCoord = {x:number,y:number,z:number,w:number};
+export class Pocket4Dimension {
+    private activeCubes = new Map<string, D174DCoord>();
+    constructor() {
+    }
+
+    initializeWith2D(rawStr: string) {
+        rawStr.split("\n").forEach((line, y) => {
+            (line.split("") as D17VisibleCubeState[]).forEach((cell, x) => {
+                if(cell === "#") {
+                    Pocket4Dimension.setState(this.activeCubes, {x,y,z:0,w:0}, "active");
+                }
+            })
+        })
+    }
+
+    performNewCycle() {
+        const buildingMap = new Map(this.activeCubes);
+
+        const { neighborCoords, uniqueNeighbors } = Pocket4Dimension.buildNeighborCoordsAround(this.activeCubes);
+
+        // Active => Inactive state
+        neighborCoords.forEach((entry, activeCoord) => {
+            const activeNeighbors = entry.neighbors.filter(coord => this.activeCubes.has(Pocket4Dimension.coordToMapKey(coord))).length;
+            if(activeNeighbors!==2 && activeNeighbors!==3) {
+                Pocket4Dimension.setState(buildingMap, entry.coord, "inactive");
+            }
+        })
+        // Inactive => Active state
+        uniqueNeighbors.forEach((neighborEntry, neighborKey) => {
+            const activeNeighborsAround = neighborEntry.activeNeighbors.length;
+            if(activeNeighborsAround === 3) {
+                Pocket4Dimension.setState(buildingMap, neighborEntry.coord, "active");
+            }
+        })
+
+        this.activeCubes = buildingMap;
+    }
+
+    activeCubesCount() {
+        return this.activeCubes.size;
+    }
+
+    static buildNeighborCoordsAround(activeCubes: Map<string, D174DCoord>) {
+        const neighborCoords = new Map<string, {coord: D174DCoord, neighbors: D174DCoord[]}>();
+        const uniqueNeighbors = new Map<string, { coord: D174DCoord, activeNeighbors: D174DCoord[]}>();
+        activeCubes.forEach((activeCoord, activeCoordKey) => {
+            const currentActiveCoordNeighbors = [] as D174DCoord[];
+            cartesian([-1,0,1],[-1,0,1],[-1,0,1], [-1,0,1]).map(([xStep,yStep,zStep,wStep]) => {
+                if(xStep!==0 || yStep!==0 || zStep!==0 || wStep!==0) {
+                    let neighborCoord = { x: activeCoord.x+xStep, y: activeCoord.y+yStep, z: activeCoord.z+zStep, w: activeCoord.w+wStep };
+                    currentActiveCoordNeighbors.push(neighborCoord);
+
+                    let neighborCoordKey = Pocket4Dimension.coordToMapKey(neighborCoord);
+                    const uniqueNeighborEntry = (uniqueNeighbors.get(neighborCoordKey) || { coord: neighborCoord, activeNeighbors: [] }) as {coord: D174DCoord, activeNeighbors: D174DCoord[]};
+                    uniqueNeighborEntry.activeNeighbors.push(activeCoord);
+                    uniqueNeighbors.set(neighborCoordKey, uniqueNeighborEntry);
+                }
+            })
+            neighborCoords.set(activeCoordKey, { coord: activeCoord, neighbors: currentActiveCoordNeighbors });
+        });
+        return { neighborCoords, uniqueNeighbors };
+    }
+
+    static setState(activeCubesMap: Map<string, D174DCoord>, coord: D174DCoord, cubeState: D17CubeState) {
+        if(cubeState==="active") {
+            activeCubesMap.set(Pocket4Dimension.coordToMapKey(coord), coord);
+        } else if(cubeState==="inactive") {
+            activeCubesMap.delete(Pocket4Dimension.coordToMapKey(coord));
+        } else {
+            throw new Error(`Unexpected cube state ; ${cubeState}`);
+        }
+    }
+
+    static coordToMapKey(coord: D174DCoord) {
+        return `${coord.x}_${coord.y}_${coord.z}_${coord.w}`;
+    }
+}
--- test/2020-17.test.ts
+++ test/2020-17.test.ts
@@ -1,4 +1,4 @@
-import {Pocket3Dimension} from "../src/2020-17";
+import {Pocket3Dimension, Pocket4Dimension} from "../src/2020-17";
 import {D17_INPUT, D17_Q1Sample} from "./2020-17.inputs";
 
 
@@ -23,3 +23,26 @@ test("Q1", () => {
 
     expect(pocketDimension.activeCubesCount()).toEqual(380);
 })
+
+test("Q2 sample", () => {
+    const pocketDimension = new Pocket4Dimension();
+    pocketDimension.initializeWith2D(D17_Q1Sample);
+
+    for(var i=0; i<6; i++) {
+        pocketDimension.performNewCycle();
+    }
+
+    expect(pocketDimension.activeCubesCount()).toEqual(848);
+})
+
+test("Q2", () => {
+    const pocketDimension = new Pocket4Dimension();
+    pocketDimension.initializeWith2D(D17_INPUT);
+
+    for(var i=0; i<6; i++) {
+        pocketDimension.performNewCycle();
+    }
+
+    expect(pocketDimension.activeCubesCount()).toEqual(2332);
+})
+
