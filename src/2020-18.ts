c4305f36035f0768b49acc34e833ef2443a3a8da
fcamblor <fcamblor@gmail.com>
2020-12-18 08:59:38 +0100

    day18 part2

--- src/2020-18.ts
+++ src/2020-18.ts
@@ -3,12 +3,16 @@
 type Operator = "+"|"*";
 
 export class D18Maths {
-    public static sumAll(str: string) {
-        return str.split("\n").reduce((result, line) => result + D18Maths.evaluate(line), 0);
+    public static sumAll(str: string, parseAndEvaluator: (line: string) => number = D18Maths.parseAndEvaluate) {
+        return str.split("\n").reduce((result, line) => result + parseAndEvaluator(line), 0);
     }
 
-    public static evaluate(str: string): number {
-        let operatorsAndOperands = str.replace(/\(/g, "( ").replace(/\)/g, " )").split(" ");
+    public static parseAndEvaluate(line: string): number {
+        let operatorsAndOperands = D18Maths.extractOperatorsAndOperandsFrom(line);
+        return D18Maths.evaluate(operatorsAndOperands);
+    }
+
+    public static evaluate(operatorsAndOperands: string[]) {
         return operatorsAndOperands.reduce(({ stack }, operatorOrOperand) => {
             if(operatorOrOperand === "+" || operatorOrOperand === "*") {
                 stack[stack.length-1].latestOperator = operatorOrOperand;
@@ -38,6 +42,76 @@ export class D18Maths {
 
             return { stack };
         }, { stack: [{ accumulator: undefined, latestOperator: undefined }] } as { stack: { accumulator: number|undefined, latestOperator: Operator|undefined }[] }).stack[0].accumulator!;
+    }
+
+    public static extractOperatorsAndOperandsFrom(line: string) {
+        return line.replace(/\(/g, "( ").replace(/\)/g, " )").split(" ");
+    }
+
+    public static addParenthesisForPlusPrecedence(line: string): string[] {
+        let operatorsAndOperands = D18Maths.extractOperatorsAndOperandsFrom(line);
+        for(var i=0; i<operatorsAndOperands.length; i++) {
+            if(operatorsAndOperands[i] === "+") {
+                // Trying to locate previous index where to insert a "("
+                let startParenthesisInsertionIndexCandidate = i-1, startParenthesisInsertionIndex = undefined, endParenthesisCount = 0;
+                while(startParenthesisInsertionIndex === undefined && startParenthesisInsertionIndexCandidate >= 0) {
+                    let value = operatorsAndOperands[startParenthesisInsertionIndexCandidate];
+                    if(value === ")") {
+                        endParenthesisCount++;
+                    } else if(value === "(") {
+                        endParenthesisCount--;
+                        if(endParenthesisCount === 0) {
+                            startParenthesisInsertionIndex = startParenthesisInsertionIndexCandidate;
+                        }
+                    } else if(["*","+"].includes(value)) {
+                        // Do nothing
+                    } else { // We're on a number
+                        if(endParenthesisCount === 0) {
+                            startParenthesisInsertionIndex = startParenthesisInsertionIndexCandidate;
+                        }
+                    }
+                    startParenthesisInsertionIndexCandidate--;
+                }
+                if(startParenthesisInsertionIndex === undefined) {
+                    startParenthesisInsertionIndex = 0;
+                }
+
+                operatorsAndOperands.splice(startParenthesisInsertionIndex, 0, "(");
+                i++;
+
+                // Same than previously, but to locate next index where to insert a ")"
+                let endParenthesisInsertionIndexCandidate = i+1, endParenthesisInsertionIndex = undefined, startParenthesisCount = 0;
+                while(endParenthesisInsertionIndex === undefined && endParenthesisInsertionIndexCandidate < operatorsAndOperands.length) {
+                    let value = operatorsAndOperands[endParenthesisInsertionIndexCandidate];
+                    if(value === "(") {
+                        startParenthesisCount++;
+                    } else if(value === ")") {
+                        startParenthesisCount--;
+                        if(startParenthesisCount === 0) {
+                            endParenthesisInsertionIndex = endParenthesisInsertionIndexCandidate;
+                        }
+                    } else if(["*","+"].includes(value)) {
+                        // Do nothing
+                    } else { // We're on a number
+                        if(startParenthesisCount === 0) {
+                            endParenthesisInsertionIndex = endParenthesisInsertionIndexCandidate;
+                        }
+                    }
+                    endParenthesisInsertionIndexCandidate++;
+                }
+                if(endParenthesisInsertionIndex === undefined) {
+                    endParenthesisInsertionIndex = operatorsAndOperands.length-1;
+                }
+
+                operatorsAndOperands.splice(endParenthesisInsertionIndex+1, 0, ")");
+                i++;
+            }
+        }
+        return operatorsAndOperands;
+    }
+
+    public static parseAndEvaluate2(str: string): number {
+        return D18Maths.evaluate(D18Maths.addParenthesisForPlusPrecedence(str));
     }
 
 
--- test/2020-18.test.ts
+++ test/2020-18.test.ts
@@ -3,12 +3,32 @@ import {D18_INPUT} from "./2020-18.inputs";
 
 
 test("Q1 samples", () => {
-    expect(D18Maths.evaluate("2 * 3 + (4 * 5)")).toEqual(26);
-    expect(D18Maths.evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toEqual(437);
-    expect(D18Maths.evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toEqual(12240);
-    expect(D18Maths.evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toEqual(13632);
+    expect(D18Maths.parseAndEvaluate("2 * 3 + (4 * 5)")).toEqual(26);
+    expect(D18Maths.parseAndEvaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toEqual(437);
+    expect(D18Maths.parseAndEvaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toEqual(12240);
+    expect(D18Maths.parseAndEvaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toEqual(13632);
 })
 
 test("Q1", () => {
     expect(D18Maths.sumAll(D18_INPUT)).toEqual(21022630974613);
+})
+
+test("addParenthesisForPlusPrecedence() on samples", () => {
+    expect(D18Maths.addParenthesisForPlusPrecedence("1 + (2 * 3) + (4 * (5 + 6))").join("")).toEqual("((1+(2*3))+(4*((5+6))))");
+    expect(D18Maths.addParenthesisForPlusPrecedence("2 * 3 + (4 * 5)").join("")).toEqual("2*(3+(4*5))");
+    expect(D18Maths.addParenthesisForPlusPrecedence("5 + (8 * 3 + 9 + 3 * 4 * 3)").join("")).toEqual("(5+(8*((3+9)+3)*4*3))");
+    expect(D18Maths.addParenthesisForPlusPrecedence("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").join("")).toEqual("5*9*(7*3*(3+9)*(3+((8+6)*4)))");
+    expect(D18Maths.addParenthesisForPlusPrecedence("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").join("")).toEqual("(((((2+4)*9)*(((6+9)*(8+6))+6))+2)+4)*2");
+})
+
+test("Q2 samples", () => {
+    expect(D18Maths.parseAndEvaluate2("1 + (2 * 3) + (4 * (5 + 6))")).toEqual(51);
+    expect(D18Maths.parseAndEvaluate2("2 * 3 + (4 * 5)")).toEqual(46);
+    expect(D18Maths.parseAndEvaluate2("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toEqual(1445);
+    expect(D18Maths.parseAndEvaluate2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toEqual(669060);
+    expect(D18Maths.parseAndEvaluate2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toEqual(23340);
+})
+
+test("Q2", () => {
+    expect(D18Maths.sumAll(D18_INPUT, D18Maths.parseAndEvaluate2)).toEqual(169899524778212);
 })
