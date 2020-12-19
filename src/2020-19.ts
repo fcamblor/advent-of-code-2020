cadcce7ac92c62a003de73794a633d4b9087d08a
fcamblor <fcamblor@gmail.com>
2020-12-19 15:29:51 +0100

    refactored in order to introduce ResolvedRule and UnresolvedRule classes

--- src/2020-19.ts
+++ src/2020-19.ts
@@ -3,12 +3,97 @@ import {cartesian} from "./utils";
 
 type D19RuleId = number;
 
-export class D19Rule {
-    constructor(public readonly id: D19RuleId, private resolvedValues: Set<string>|undefined, private readonly possibleReferences1: D19RuleId[]|undefined, private readonly possibleReferences2: D19RuleId[]|undefined) {
+export abstract class D19Rule {
+    constructor(public readonly id: D19RuleId) {
     }
 
-    isResolved() {
-        return this.resolvedValues !== undefined;
+    isResolved(): this is D19ResolvedRule {
+        return this instanceof D19ResolvedRule;
+    }
+    isNotResolved(): this is D19UnresolvedRule {
+        return !this.isResolved();
+    }
+
+    abstract resolutionStatus(rules: Map<D19RuleId, D19Rule>): object;
+
+    public static resolveRules(rules: D19Rule[]) {
+        const resolvedRules = rules.reduce((resolvedRulesById, rule) => {
+            if(rule.isResolved()) {
+                resolvedRulesById.set(rule.id, rule);
+            }
+            return resolvedRulesById;
+        }, new Map<D19RuleId, D19ResolvedRule>())
+        let resolvableRules = rules.filter(rule => rule.isNotResolved() && rule.missinRequiredDependencies(resolvedRules).length===0) as D19UnresolvedRule[];
+        let unresolvableRules = rules.filter(rule => rule.isNotResolved() && rule.missinRequiredDependencies(resolvedRules).length!==0) as D19UnresolvedRule[];
+
+        while((unresolvableRules.length + resolvableRules.length) > 0) {
+            // D19Rule.debug(rules);
+            if(!resolvableRules.length) {
+                throw new Error("No remaining resolvable rule found !");
+            }
+
+            const resolvableRule = resolvableRules.pop()!;
+            const resolvedRule = resolvableRule.resolveWith(resolvedRules);
+            resolvedRules.set(resolvedRule.id, resolvedRule);
+
+            const newResolvableRules = unresolvableRules.filter(rule => rule.missinRequiredDependencies(resolvedRules).length===0);
+            unresolvableRules = unresolvableRules.filter(rule => rule.missinRequiredDependencies(resolvedRules).length !== 0);
+            resolvableRules = resolvableRules.concat(newResolvableRules);
+        }
+
+        return { resolvedRules };
+    }
+
+    public static createFromLine(line: string) {
+        let match = line.match(/^([0-9]+): "([a-z]+)"$/);
+        if(match) {
+            return new D19ResolvedRule(Number(match[1]), new Set([ match[2] ]));
+        }
+
+        // Don't know why but it doesn't work...
+        // match = line.match(/^([0-9]+):(?:\s([^\s]+))+$/g)
+        match = line.match(/^([0-9]+): ([^|]+)$/)
+        if(match) {
+            const ruleIds = match[2].split(" ").map(Number) as D19RuleId[];
+            return new D19UnresolvedRule(Number(match[1]), ruleIds, undefined);
+        }
+
+        // Don't know why but it doesn't work...
+        // match = line.match(/^([0-9]+): (.+) | (.+)$/)
+        match = line.match(/^([0-9]+): (.+)$/)
+        if(match) {
+            const [ruleIds1Str, ruleIds2Str] = match[2].split("|").map(str => str.trim());
+            const ruleIds1 = ruleIds1Str.split(" ").map(Number) as D19RuleId[];
+            const ruleIds2 = ruleIds2Str.split(" ").map(Number) as D19RuleId[];
+            return new D19UnresolvedRule(Number(match[1]), ruleIds1, ruleIds2);
+        }
+
+        throw new Error(`Non parsable rule line : ${line}`);
+    }
+
+    public static createFromRaw(rawStr: string) {
+        return rawStr.split("\n").map(D19Rule.createFromLine).reduce((rules, rule) => rules.concat(rule), [] as D19Rule[]);
+    }
+
+    public static debug(rules: D19Rule[], resolvedRules: Map<D19RuleId, D19ResolvedRule>) {
+        const resolutionStatuses = rules.map(rule => rule.resolutionStatus(resolvedRules));
+        console.log(resolutionStatuses);
+    }
+}
+
+export class D19UnresolvedRule extends D19Rule {
+    constructor(id: D19RuleId, public readonly possibleReferences1: D19RuleId[], public readonly possibleReferences2: D19RuleId[]|undefined) {
+        super(id);
+    }
+
+    resolutionStatus(rules: Map<D19RuleId, D19Rule>) {
+        const possibleValues = this.mapPossibleReferences(possibleReferences => possibleReferences.map(ref => "" + ref + (rules.get(ref)!.isResolved()?"*":"")).join(" ")).join(" | ");
+
+        return {id: this.id, resolutions: undefined, possibleValues };
+    }
+
+    private mapPossibleReferences<T>(mapper: (possibleReferences: D19RuleId[]) => T): T[] {
+        return [this.possibleReferences1, this.possibleReferences2].filter(possibleReferences => !!possibleReferences).map(possibleRef => mapper(possibleRef!));
     }
 
     requiredUniqueDependencies() {
@@ -21,120 +106,50 @@ export class D19Rule {
         return Array.from(ruleIds);
     }
 
-    missinRequiredDependencies(rules: Map<D19RuleId, D19Rule>) {
+    missinRequiredDependencies(resolvedRules: Map<D19RuleId, D19ResolvedRule>) {
         return this.requiredUniqueDependencies().reduce((missingRequiredDependencies, requiredDependencyId) => {
-            return missingRequiredDependencies.concat(rules.get(requiredDependencyId)!.isResolved() ? [] : [ requiredDependencyId ] );
+            return missingRequiredDependencies.concat(resolvedRules.has(requiredDependencyId) ? [] : [ requiredDependencyId ] );
         }, [] as D19RuleId[]);
     }
 
-    allRequiredDependenciesAreResolved(rules: Map<D19RuleId, D19Rule>) {
-        return this.missinRequiredDependencies(rules).length === 0;
+    allRequiredDependenciesAreResolved(resolvedRules: Map<D19RuleId, D19ResolvedRule>) {
+        return this.missinRequiredDependencies(resolvedRules).length === 0;
     }
 
-    resolveWith(rules: Map<D19RuleId, D19Rule>) {
-        if(!this.allRequiredDependenciesAreResolved(rules)) {
-            throw new Error(`Trying to resolve a rule with unresolved dependencies ! (ruleId=${this.id}, unresolvedDeps=${this.missinRequiredDependencies(rules).join(" ")})`);
+    resolveWith(resolvedRules: Map<D19RuleId, D19ResolvedRule>) {
+        if(!this.allRequiredDependenciesAreResolved(resolvedRules)) {
+            throw new Error(`Trying to resolve a rule with unresolved dependencies ! (ruleId=${this.id}, unresolvedDeps=${this.missinRequiredDependencies(resolvedRules).join(" ")})`);
         }
 
         const perPossibleReferenceValues = this.mapPossibleReferences(possibleReferences => {
-            return cartesian(...possibleReferences.map(ruleId => Array.from(rules.get(ruleId)!.resolvedValues!))).map(values => values.join(''));
+            return cartesian(...possibleReferences.map(ruleId => Array.from(resolvedRules.get(ruleId)!.resolvedValues!))).map(values => values.join(''));
         });
 
         const resolvedValues = new Set<string>();
         perPossibleReferenceValues.forEach(possibleValues => possibleValues.forEach(possibleValue => resolvedValues.add(possibleValue)))
-        this.resolvedValues = resolvedValues;
+        return new D19ResolvedRule(this.id, resolvedValues);
     }
+}
 
-    matchesWith(candidate: string): boolean {
-        if(!this.isResolved()) {
-            throw new Error(`You shouldn't call matchesWith() on an unresolved rule (ruleId=${this.id})`);
-        }
-
-        return this.resolvedValues!.has(candidate);
-    }
-
-    stringified() {
-        return JSON.stringify({
-            id: this.id,
-            resolvedValues: (this.resolvedValues!==undefined)?Array.from(this.resolvedValues):undefined,
-            possibleReferences1: this.possibleReferences1,
-            possibleReferences2: this.possibleReferences2,
-        });
+export class D19ResolvedRule extends D19Rule {
+    constructor(id: D19RuleId, public readonly resolvedValues: Set<string>) {
+        super(id)
     }
 
     resolutionStatus(rules: Map<D19RuleId, D19Rule>) {
         const resolutions = this.isResolved()?Array.from(this.resolvedValues!):undefined;
-        const possibleValues = this.mapPossibleReferences(possibleReferences => possibleReferences.map(ref => "" + ref + (rules.get(ref)!.isResolved()?"*":"")).join(" ")).join(" | ");
 
-        return {id: this.id, resolutions, possibleValues };
+        return {id: this.id, resolutions };
     }
 
-    private mapPossibleReferences<T>(mapper: (possibleReferences: D19RuleId[]) => T): T[] {
-        return [this.possibleReferences1, this.possibleReferences2].filter(possibleReferences => !!possibleReferences).map(possibleRef => mapper(possibleRef!));
-    }
-
-    public static resolveRules(rules: Map<D19RuleId, D19Rule>) {
-        let resolvableRules = Array.from(rules.values()).filter(rule => !rule.isResolved() && rule.missinRequiredDependencies(rules).length===0);
-        let unresolvableRules = Array.from(rules.values()).filter(rule => rule.missinRequiredDependencies(rules).length!==0);
-
-        while((unresolvableRules.length + resolvableRules.length) > 0) {
-            // D19Rule.debug(rules);
-            if(!resolvableRules.length) {
-                throw new Error("No remaining resolvable rule found !");
-            }
-
-            const resolvableRule = resolvableRules.pop()!;
-            resolvableRule.resolveWith(rules);
-
-            const newResolvableRules = unresolvableRules.filter(rule => rule.missinRequiredDependencies(rules).length===0);
-            unresolvableRules = unresolvableRules.filter(rule => rule.missinRequiredDependencies(rules).length !== 0);
-            resolvableRules = resolvableRules.concat(newResolvableRules);
-        }
-    }
-
-    public static createFromLine(line: string) {
-        let match = line.match(/^([0-9]+): "([a-z]+)"$/);
-        if(match) {
-            return new D19Rule(Number(match[1]), new Set([ match[2] ]), undefined, undefined);
-        }
-
-        // Don't know why but it doesn't work...
-        // match = line.match(/^([0-9]+):(?:\s([^\s]+))+$/g)
-        match = line.match(/^([0-9]+): ([^|]+)$/)
-        if(match) {
-            const ruleIds = match[2].split(" ").map(Number) as D19RuleId[];
-            return new D19Rule(Number(match[1]), undefined, ruleIds, undefined);
-        }
-
-        // Don't know why but it doesn't work...
-        // match = line.match(/^([0-9]+): (.+) | (.+)$/)
-        match = line.match(/^([0-9]+): (.+)$/)
-        if(match) {
-            const [ruleIds1Str, ruleIds2Str] = match[2].split("|").map(str => str.trim());
-            const ruleIds1 = ruleIds1Str.split(" ").map(Number) as D19RuleId[];
-            const ruleIds2 = ruleIds2Str.split(" ").map(Number) as D19RuleId[];
-            return new D19Rule(Number(match[1]), undefined, ruleIds1, ruleIds2);
-        }
-
-        throw new Error(`Non parsable rule line : ${line}`);
-    }
-
-    public static createFromRaw(rawStr: string) {
-        return rawStr.split("\n").map(D19Rule.createFromLine).reduce((rulesById, rule) => {
-            rulesById.set(rule.id, rule);
-            return rulesById;
-        }, new Map<D19RuleId, D19Rule>());
-    }
-
-    public static debug(rules: Map<D19RuleId, D19Rule>) {
-        const resolutionStatuses = Array.from(rules.values()).map(rule => rule.resolutionStatus(rules));
-        console.log(resolutionStatuses);
+    matchesWith(candidate: string): boolean {
+        return this.resolvedValues!.has(candidate);
     }
 }
 
 export function countMessagesMatchingRules(rawRules: string, rawMessages: string) {
-    let rulesMap = D19Rule.createFromRaw(rawRules);
-    D19Rule.resolveRules(rulesMap);
-    const rule0 = rulesMap.get(0)!;
+    let rules = D19Rule.createFromRaw(rawRules);
+    const { resolvedRules } = D19Rule.resolveRules(rules);
+    const rule0 = resolvedRules.get(0)!;
     return rawMessages.split("\n").reduce((count, line) => count + (rule0.matchesWith(line)?1:0), 0);
 }
--- test/2020-19.test.ts
+++ test/2020-19.test.ts
@@ -1,27 +1,14 @@
-import {countMessagesMatchingRules, D19Rule} from "../src/2020-19";
+import {countMessagesMatchingRules, D19ResolvedRule, D19Rule, D19UnresolvedRule} from "../src/2020-19";
 import {D19_INPUT, D19_Q1_SAMPLE} from "./2020-19.inputs";
 
 
 test("Q1 samples parsing", () => {
-    let rulesMap = D19Rule.createFromRaw(D19_Q1_SAMPLE.rules);
-    expect(rulesMap.get(0)!.stringified()).toEqual(JSON.stringify(
-        {id: 0, resolvedValues: undefined, possibleReferences1: [4, 1, 5], possibleReferences2: undefined}
-    ));
-    expect(rulesMap.get(1)!.stringified()).toEqual(JSON.stringify(
-        {id: 1, resolvedValues: undefined, possibleReferences1: [2, 3], possibleReferences2: [3, 2]}
-    ));
-    expect(rulesMap.get(2)!.stringified()).toEqual(JSON.stringify(
-        {id: 2, resolvedValues: undefined, possibleReferences1: [4, 4], possibleReferences2: [5, 5]}
-    ));
-    expect(rulesMap.get(3)!.stringified()).toEqual(JSON.stringify(
-        {id: 3, resolvedValues: undefined, possibleReferences1: [4, 5], possibleReferences2: [5, 4]}
-    ));
-    expect(rulesMap.get(4)!.stringified()).toEqual(JSON.stringify(
-        {id: 4, resolvedValues: ["a"], possibleReferences1: undefined, possibleReferences2: undefined}
-    ));
-    expect(rulesMap.get(5)!.stringified()).toEqual(JSON.stringify(
-        {id: 5, resolvedValues: ["b"], possibleReferences1: undefined, possibleReferences2: undefined}
-    ));
+    let rules = D19Rule.createFromRaw(D19_Q1_SAMPLE.rules);
+    expect(rules[0]).toEqual(new D19UnresolvedRule(0, [4, 1, 5], undefined));
+    expect(rules[1]).toEqual(new D19UnresolvedRule(1, [2, 3], [3, 2]));
+    expect(rules[2]).toEqual(new D19UnresolvedRule(2, [4, 4], [5, 5]));
+    expect(rules[4]).toEqual(new D19ResolvedRule(4, new Set("a")));
+    expect(rules[5]).toEqual(new D19ResolvedRule(5, new Set("b")));
 })
 
 test("INPUT parsing doesn't produce any error", () => {
