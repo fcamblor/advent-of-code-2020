e0eed66f783f77eba79ab468404f078aa66dc24c
fcamblor <fcamblor@gmail.com>
2020-12-22 17:03:12 +0100

    d22 part2 not that clean impl .. I'd better refactor it in a more OO way

--- src/2020-22.ts
+++ src/2020-22.ts
@@ -6,7 +6,8 @@ export class D22Player {
     constructor(public readonly name: string, public readonly deck: D20Card[]) {
     }
 
-    deckNotEmpty(){ return !!this.deck.length; }
+    deckNotEmpty(){ return !this.deckIsEmpty(); }
+    deckIsEmpty(){ return this.deck.length===0; }
     playNextCard() { return this.deck.shift()!; }
     addCardsToDeck([c1, c2]: [ D20Card, D20Card ]) {
         this.deck.push(...[c1, c2])
@@ -16,43 +17,175 @@ export class D22Player {
             return score + card*(this.deck.length-index);
         }, 0)
     }
+
+    cloneWithDeckSizeOf(deckSize: number) {
+        return new ClonedD22Player(this.name, [...this.deck.slice(0, deckSize)], this);
+    }
+}
+
+export class ClonedD22Player extends D22Player {
+    constructor(name: string, deck: D20Card[], public readonly clonedPlayer: D22Player) {
+        super(name, deck)
+    }
 }
 
 export class D22CombatGame {
-    constructor(private readonly player1: D22Player, private readonly player2: D22Player) {
-    }
 
-
-    play() {
+    static play(player1: D22Player, player2: D22Player) {
+        let currentGame = { player1: player1, player2: player2, gameId: 1 }
         let round = 1;
-        while(this.player1.deckNotEmpty() && this.player2.deckNotEmpty()) {
-            const card1 = this.player1.playNextCard();
-            const card2 = this.player2.playNextCard();
+        while(currentGame.player1.deckNotEmpty() && currentGame.player2.deckNotEmpty()) {
+            const card1 = currentGame.player1.playNextCard();
+            const card2 = currentGame.player2.playNextCard();
 
             if(card1 > card2) {
-                this.player1.addCardsToDeck([ card1, card2 ]);
+                currentGame.player1.addCardsToDeck([ card1, card2 ]);
             } else {
-                this.player2.addCardsToDeck([ card2, card1 ]);
+                currentGame.player2.addCardsToDeck([ card2, card1 ]);
             }
 
-            turn++;
+            round++;
         }
 
-        return this;
+        const winner = currentGame.player1.deckNotEmpty()?currentGame.player1:currentGame.player2;
+        return { winner, outputs: ['']};
+    }
+}
+
+type D22RecursiveCombatGame = {player1:D22Player, player2:D22Player, gameId: number, round: number, card1: number, card2: number, uniqueRoundPrevention: Set<string>};
+type D22RecursiveCombatGameState = { gamesStack: D22RecursiveCombatGame[], nextGameId: number, latestGame: D22RecursiveCombatGame|undefined };
+export class D22RecursiveCombat {
+
+    static stackNewGame(state: D22RecursiveCombatGameState, gameInfos: Omit<D22RecursiveCombatGame, "gameId"|"card1"|"card2"|"uniqueRoundPrevention">): D22RecursiveCombatGame  {
+        let newGame: D22RecursiveCombatGame = {...gameInfos, gameId: state.nextGameId, card1: -1, card2: -1, uniqueRoundPrevention: new Set<string>() };
+        state.gamesStack.push(newGame);
+        state.nextGameId++;
+        return newGame;
     }
 
+    static play(player1: D22Player, player2: D22Player) {
+        // For debug purposes, change this value to a specific value to look for infinite loops
+        const MAX_NUMBER_OF_OVERALL_ROUNDS = Infinity;
+        let overallRound = 1;
 
-    winnerScore() {
-        const winner = this.player1.canPlay()?this.player1:this.player2;
-        return winner.score();
+        const outputs = [];
+        const recursiveGameState: D22RecursiveCombatGameState = { gamesStack: [], nextGameId: 1, latestGame: undefined };
+        let currentGame: D22RecursiveCombatGame|undefined = D22RecursiveCombat.stackNewGame(recursiveGameState, { player1, player2, round: 1 });
+
+        outputs.push(`=== Game ${currentGame.gameId} ===`)
+        outputs.push(``)
+
+        while(currentGame !== undefined && overallRound<MAX_NUMBER_OF_OVERALL_ROUNDS) {
+            let gameChecksum = `g:${currentGame.gameId};p1:${currentGame.player1.deck.join(",")};p2:${currentGame.player2.deck.join(",")}`
+            while(currentGame !== undefined && currentGame.uniqueRoundPrevention.has(gameChecksum)) {
+                const gameWinner = currentGame.player1;
+                currentGame = D22RecursiveCombat.declareGameWinner(gameWinner, currentGame, recursiveGameState, outputs);
+                if (currentGame !== undefined) {
+                    gameChecksum = `g:${currentGame.gameId};p1:${currentGame.player1.deck.join(",")};p2:${currentGame.player2.deck.join(",")}`
+                }
+            }
+            if(currentGame === undefined) {
+                break;
+            }
+            currentGame.uniqueRoundPrevention.add(gameChecksum);
+
+            outputs.push(`-- Round ${currentGame.round} (Game ${currentGame.gameId}) --`);
+            outputs.push(`${currentGame.player1.name}'s deck: ${currentGame.player1.deck.join(", ")}`);
+            outputs.push(`${currentGame.player2.name}'s deck: ${currentGame.player2.deck.join(", ")}`);
+
+            currentGame.card1 = currentGame.player1.playNextCard();
+            currentGame.card2 = currentGame.player2.playNextCard();
+
+            outputs.push(`${currentGame.player1.name} plays: ${currentGame.card1}`);
+            outputs.push(`${currentGame.player2.name} plays: ${currentGame.card2}`);
+
+            if(currentGame.card1 <= currentGame.player1.deck.length && currentGame.card2 <= currentGame.player2.deck.length) {
+                currentGame = D22RecursiveCombat.stackNewGame(recursiveGameState, {
+                    player1: currentGame.player1.cloneWithDeckSizeOf(currentGame.card1),
+                    player2: currentGame.player2.cloneWithDeckSizeOf(currentGame.card2),
+                    round: 0,
+                });
+
+                outputs.push("Playing a sub-game to determine the winner...");
+
+                outputs.push(``)
+                outputs.push(`=== Game ${currentGame.gameId} ===`)
+            } else {
+                let roundWinner;
+                if(currentGame.card1 > currentGame.card2) {
+                    currentGame.player1.addCardsToDeck([ currentGame.card1, currentGame.card2 ]);
+                    roundWinner = currentGame.player1;
+                } else {
+                    currentGame.player2.addCardsToDeck([ currentGame.card2, currentGame.card1 ]);
+                    roundWinner = currentGame.player2;
+                }
+                outputs.push(`${roundWinner.name} wins round ${currentGame.round} of game ${currentGame.gameId}!`);
+
+                while(currentGame !== undefined && (currentGame.player1.deckIsEmpty() || currentGame.player2.deckIsEmpty())) {
+                    const gameWinner = currentGame.player1.deckIsEmpty()?currentGame.player2:currentGame.player1;
+                    currentGame = D22RecursiveCombat.declareGameWinner(gameWinner, currentGame, recursiveGameState, outputs);
+                }
+
+                if(currentGame === undefined) {
+                    break;
+                }
+            }
+
+            currentGame.round++;
+            overallRound++;
+
+            outputs.push(``);
+        }
+
+        return D22RecursiveCombat.endOfGame(recursiveGameState.latestGame, outputs);
     }
 
-    static createFrom(str: string) {
-        const [ player1, player2 ] = str.split("\n\n").map(str => str.trim()).map(playerStr => {
-            const [ name, ...cards ] = playerStr.split("\n");
-            return new D22Player(name.split(":")[0], cards.map(Number));
-        })
+    private static declareGameWinner(gameWinner: D22Player, currentGame: D22RecursiveCombatGame, recursiveGameState: D22RecursiveCombatGameState, outputs: string[]) {
+        // toLowerCase() here shows that test is not really taking into account true player name here unfortunately !
+        outputs.push(`The winner of game ${currentGame.gameId} is ${gameWinner.name.toLowerCase()}!`)
+        outputs.push(``)
 
-        return new D22CombatGame(player1, player2);
+        recursiveGameState.latestGame = recursiveGameState.gamesStack.pop();
+        const previouslyStackedGame = recursiveGameState.gamesStack.length===0?undefined:recursiveGameState.gamesStack[recursiveGameState.gamesStack.length-1];
+        if(previouslyStackedGame !== undefined) {
+            const clonedGameWinner = gameWinner as ClonedD22Player;
+            outputs.push(`...anyway, back to game ${previouslyStackedGame.gameId}.`)
+            outputs.push(`${gameWinner.name} wins round ${previouslyStackedGame.round} of game ${previouslyStackedGame.gameId}!`);
+
+            // Not sure if the problem statement is very clear on this...
+            // clonedGameWinner.clonedPlayer.addCardsToDeck([ Math.min(...[previouslyStackedGame.card1, previouslyStackedGame.card2]), Math.max(...[previouslyStackedGame.card1, previouslyStackedGame.card2]) ]);
+            if(clonedGameWinner.clonedPlayer.name === "Player 1") {
+                clonedGameWinner.clonedPlayer.addCardsToDeck([ previouslyStackedGame.card1, previouslyStackedGame.card2 ]);
+            } else {
+                clonedGameWinner.clonedPlayer.addCardsToDeck([ previouslyStackedGame.card2, previouslyStackedGame.card1 ]);
+            }
+        }
+        return previouslyStackedGame;
     }
+
+    private static endOfGame(game: D22RecursiveCombatGame|undefined, outputs: string[], forcedWinner: D22Player|undefined = undefined) {
+        let winner = undefined;
+        if(game !== undefined){
+            outputs.push(``)
+            outputs.push(`== Post-game results ==`)
+            outputs.push(`${game.player1.name}'s deck: ${game.player1.deck.join(", ")}`)
+            outputs.push(`${game.player2.name}'s deck: ${game.player2.deck.join(", ")}`)
+
+            if(forcedWinner === undefined) {
+                winner = game.player1.deckNotEmpty()?game.player1:game.player2;
+            } else {
+                winner = forcedWinner;
+            }
+        }
+        return { winner, outputs: outputs };
+    }
+}
+
+export function d22play(str: string, game: (player1: D22Player, player2: D22Player) => {winner:D22Player|undefined, outputs: string[]}): {winner:D22Player|undefined, outputs: string[]} {
+    const [ player1, player2 ] = str.split("\n\n").map(str => str.trim()).map(playerStr => {
+        const [ name, ...cards ] = playerStr.split("\n");
+        return new D22Player(name.split(":")[0], cards.map(Number));
+    })
+
+    return game(player1, player2);
 }
--- test/2020-22.test.ts
+++ test/2020-22.test.ts
@@ -1,11 +1,38 @@
-import {D22CombatGame} from "../src/2020-22";
-import {D22_INPUT, D22_Q1_SAMPLE} from "./2020-22.inputs";
+import {D22CombatGame, d22play, D22RecursiveCombat} from "../src/2020-22";
+import {D22_INPUT, D22_Q1_SAMPLE, D22_SAMPLE_Q2_EXPECTED_OUTPUT} from "./2020-22.inputs";
 
 
 test("Q1 sample", () => {
-    expect(D22CombatGame.createFrom(D22_Q1_SAMPLE).play().winnerScore()).toEqual(306);
+    expect(d22play(D22_Q1_SAMPLE, D22CombatGame.play).winner!.score()).toEqual(306);
 })
 
 test("Q1 INPUT", () => {
-    expect(D22CombatGame.createFrom(D22_INPUT).play().winnerScore()).toEqual(32598);
+    expect(d22play(D22_INPUT, D22CombatGame.play).winner!.score()).toEqual(32598);
+})
+
+test("Q2 sample", () => {
+    let gameOutcome = d22play(D22_SAMPLE.rawString, D22RecursiveCombat.play);
+    // console.log(gameOutcome.outputs.join("\n"))
+    expect(gameOutcome.outputs).toEqual(D22_SAMPLE.expectedQ2Output.split("\n"));
+    expect(gameOutcome.winner!.score()).toEqual(291);
+})
+
+test("Q2 game is not looping forever", () => {
+    const loopingCandidateInput = `
+Player 1:
+43
+19
+
+Player 2:
+2
+29
+14
+    `.trim()
+    let gameOutcome = d22play(loopingCandidateInput, D22RecursiveCombat.play);
+    console.log(gameOutcome.outputs.join("\n"))
+})
+
+test("Q2 INPUT", () => {
+    let gameOutcome = d22play(D22_INPUT, D22RecursiveCombat.play);
+    expect(gameOutcome.winner!.score()).toEqual(35836);
 })
