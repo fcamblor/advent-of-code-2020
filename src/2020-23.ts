471131345d76d9f860c2bd97b291e9be7a68c2c4
fcamblor <fcamblor@gmail.com>
2020-12-24 02:40:41 +0100

    d23 p2 - completely refactored D23Puzzle impl, to change underlying data structure to use a Map containing next cup for a given cup

--- src/2020-23.ts
+++ src/2020-23.ts
@@ -1,59 +1,112 @@
-import {AoCLogger, reduceTimes} from "./utils";
+import {AoCLogger} from "./utils";
 
 export class D23Puzzle {
-    public crabValue: number
-    public cups: number[];
-    private maxCupValue: number;
     private moveCount: number;
-    public logger: AoCLogger;
+    private crabCup: number;
+    private maxCup: number;
+    private nextCupByCupValue: Map<number, number>;
 
-    constructor(str: string) {
-        this.cups = str.split("").map(Number);
-        this.crabValue = this.cups[0];
-        this.maxCupValue = Math.max(...this.cups);
+    private logger: AoCLogger;
+
+    constructor(str: string, logEnabled: boolean = true) {
+        this.nextCupByCupValue = new Map<number, number>();
+
+        const cups = str.split("").map(Number);
+        cups.forEach((cup, idx, cups) => {
+            this.nextCupByCupValue.set(cup, cups[(idx+1)%cups.length]);
+        })
+
+        this.crabCup = cups[0];
+        this.maxCup = Math.max(...cups);
+
         this.moveCount = 0;
+
         this.logger = new AoCLogger();
+        if(!logEnabled) {
+            this.logger.disable();
+        }
     }
 
     move(): this {
         this.moveCount++;
 
-        this.logger.append(`-- move ${this.moveCount} --`)
-        this.logger.append(`cups: (${this.cups[0]}) ${this.cups.slice(1).join(" ")}`)
+        this.logger.appendIfEnabled(() => `-- move ${this.moveCount} --`)
+        this.logger.appendIfEnabled(() => {
+            const cups = this.listCupsFrom(this.crabCup);
+            return `cups: (${cups[0]}) ${cups.slice(1).join(" ")}`
+        })
 
-        const splicedCups = this.cups.splice(1, 3);
-        this.logger.append(`pick up: ${splicedCups.join(", ")}`);
+        const firstPick = this.nextCupByCupValue.get(this.crabCup)!;
+        const secondPick = this.nextCupByCupValue.get(firstPick)!;
+        const thirdPick = this.nextCupByCupValue.get(secondPick)!;
 
-        let destinationCup = (this.crabValue + (this.maxCupValue+1) -1)%(this.maxCupValue+1);
-        while(this.cups.indexOf(destinationCup) === -1) {
-            destinationCup = (destinationCup + (this.maxCupValue+1) -1)%(this.maxCupValue+1);
-        }
-        this.logger.append(`destination: ${destinationCup}`);
+        // This removes the 3 cups above by undirecting current crab cup to cup aimed by thirdPick
+        this.nextCupByCupValue.set(this.crabCup, this.nextCupByCupValue.get(thirdPick)!);
+
+        const removedCups = [ firstPick, secondPick, thirdPick ];
+
+        this.logger.appendIfEnabled(() => `pick up: ${removedCups.join(", ")}`);
+
+        let destinationCup = this.crabCup;
+        do {
+            destinationCup--;
+            if(destinationCup === 0){
+                destinationCup = this.maxCup;
+            }
+        }while(removedCups.includes(destinationCup));
+
+        this.logger.appendIfEnabled(() => `destination: ${destinationCup}`);
         this.logger.newLine();
 
-        let destinationCupIndex = this.cups.indexOf(destinationCup);
-        this.cups.splice(destinationCupIndex+1, 0, ...splicedCups);
+        const actualDestinationNextCup = this.nextCupByCupValue.get(destinationCup)!;
 
-        this.crabValue = this.cups[1];
-        this.cups = this.rollCupsToHaveValueAtStartingIndex(this.crabValue);
+        // Re-integrating the 3 removed cups after destinationCup
+        this.nextCupByCupValue.set(destinationCup, firstPick);
+        this.nextCupByCupValue.set(thirdPick, actualDestinationNextCup);
+
+        // Moving crabcup forward
+        this.crabCup = this.nextCupByCupValue.get(this.crabCup)!;
+
+        this.logger.printAndFlush();
+
         return this;
     }
 
     moveTimes(times: number): this {
-        reduceTimes(times, () => this.move(), undefined);
+        for(let i=0; i<times; i++) {
+            this.move();
+        }
         return this;
     }
 
+    listCupsFrom(startingCup: number) {
+        const cups = [ startingCup ];
+        let currentCup = this.nextCupByCupValue.get(startingCup)!;
+        while(currentCup !== startingCup) {
+            cups.push(currentCup);
+            currentCup = this.nextCupByCupValue.get(currentCup)!;
+        }
+        return cups;
+    }
+
     currentCode(): string {
-        const cups = this.rollCupsToHaveValueAtStartingIndex(1);
+        const cups = this.listCupsFrom(1);
         cups.shift();
         return cups.join("");
     }
 
-    private rollCupsToHaveValueAtStartingIndex(value: number) {
-        const valueIndex = this.cups.indexOf(value);
-        const valuesBefore = this.cups.slice(0, valueIndex);
-        const valuesAfter = this.cups.slice(valueIndex+1);
-        return [ value, ...valuesAfter, ...valuesBefore ];
+    appendNumbersToCups(from: number, to: number): this {
+        const cups = this.listCupsFrom(this.crabCup);
+        this.nextCupByCupValue.set(cups[cups.length-1], from);
+        for(let i=from; i<to; i++) {
+            this.nextCupByCupValue.set(i, i+1);
+        }
+        this.nextCupByCupValue.set(to, this.crabCup);
+        this.maxCup = to;
+        return this;
+    }
+
+    findCupAfter(cup: number) {
+        return this.nextCupByCupValue.get(cup);
     }
 }
--- test/2020-23.test.ts
+++ test/2020-23.test.ts
@@ -1,14 +1,30 @@
 import {D23Puzzle} from "../src/2020-23";
 
-
 test("Q1 sample", () => {
     let puzzle = new D23Puzzle("389125467");
     expect(puzzle.moveTimes(10).currentCode()).toEqual("92658374");
-    // puzzle.logger.print();
-    expect(puzzle.moveTimes(90).currentCode()).toEqual("67384529");
+    expect(new D23Puzzle("389125467").moveTimes(100).currentCode()).toEqual("67384529");
 })
 
 test("Q1 INPUT", () => {
     let puzzle = new D23Puzzle("739862541");
     expect(puzzle.moveTimes(100).currentCode()).toEqual("94238657");
+})
+
+test("Q2 sample", () => {
+    let puzzle = new D23Puzzle("389125467", false).appendNumbersToCups(10, 1000000);
+    puzzle.moveTimes(10000000);
+    const cupAfter1 = puzzle.findCupAfter(1)!;
+    const cupAfterAfter1 = puzzle.findCupAfter(cupAfter1)!;
+    expect(cupAfter1).toEqual(934001)
+    expect(cupAfterAfter1).toEqual(159792)
+    expect(cupAfter1 * cupAfterAfter1).toEqual(149245887792);
+})
+
+test("Q2 INPUT", () => {
+    let puzzle = new D23Puzzle("739862541", false).appendNumbersToCups(10, 1000000);
+    puzzle.moveTimes(10000000);
+    const cupAfter1 = puzzle.findCupAfter(1)!;
+    const cupAfterAfter1 = puzzle.findCupAfter(cupAfter1)!;
+    expect(cupAfter1 * cupAfterAfter1).toEqual(3072905352);
 })
